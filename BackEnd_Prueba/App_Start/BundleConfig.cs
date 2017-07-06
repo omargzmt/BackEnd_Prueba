6cf3c10211f3e02409a3be86ccd3adf85527926c
Omar_Gapi-PC\Omar_Gapi <omar.gzmt4@gmail.com>
2017-07-06 10:51:43 -0500

    Inicio del proyecto

---
+++ BackEnd_Prueba/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BackEnd_Prueba
+{
+    public class BundleConfig
+    {
+        // Para obtener más información acerca de Bundling, consulte http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de creación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.6.2.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+               "~/Scripts/bootstrap.js",
+               "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/normalize.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+               "~/Content/bootstrap.css",                
+               "~/Content/bootstrap.min.css",
+               "~/Content/bootstrap-theme.css",
+               "~/Content/Site.css"));            
+
+
+        }
+    }
+}
---
+++ BackEnd_Prueba/Controllers/UsuarioController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BackEnd_Prueba.Models;
+
+namespace BackEnd_Prueba.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private PruebaBackEndEntities db = new PruebaBackEndEntities();
+
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View(db.usuario.ToList());
+        }
+
+        //
+        // GET: /Usuario/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Usuario/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            usuario usuario = db.usuario.Find(id);
+            db.usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
